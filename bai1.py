432beaa1bf8092ffc422ed3d38a81c27be6faf24
GiangHongDang <doratv360@gmail.com>
2026-01-23 19:22:08 +0700

    Bài tập 1

---
+++ bai1.py
@@ -0,0 +1,12 @@
+try : 
+    dtb = int(input("nhập số điểm trung bình : "))
+    if dtb >= 8.5 :
+        print ("Học sinh giỏi")
+    elif dtb >= 7 :
+        print ("Học sinh khá ")
+    elif dtb >= 5 :
+        print ("Học sinh trung bình")
+    else:
+        print (" Học sinh yếu")
+except ValueError:
+   print ("Vui lòng nhập số hợp lệ")
