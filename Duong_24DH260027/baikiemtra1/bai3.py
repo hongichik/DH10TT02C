c2c31b6bd3f03ef342eaec0245a57abf1316d359
duong-nhom10 <a@gmail.com>
2026-01-28 10:36:28 +0700

    sửa bài

---
+++ Duong_24DH260027/baikiemtra1/bai3.py
@@ -0,0 +1,10 @@
+n = int(input("Nhập một số nguyên dương: "))
+if n > 1:
+    for i in range(2, n):
+        if n % i == 0:
+            print("Không phải số nguyên tố")
+            break
+    else:
+        print("Là số nguyên tố")
+else:
+    print("Vui lòng nhập lại số lớn hơn 1")
