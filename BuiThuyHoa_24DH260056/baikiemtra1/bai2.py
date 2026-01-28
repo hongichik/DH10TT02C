1b5db81f320d59b2bd8d4da1954975b98684998f
hoa-nhom9 <h@gmail.com>
2026-01-28 10:58:00 +0700

    bai 1

---
+++ BuiThuyHoa_24DH260056/baikiemtra1/bai2.py
@@ -0,0 +1,13 @@
+password = "noptiendi"
+count = 0
+while count < 3:
+    mk = input("Nhập mật khẩu: ")
+    if mk == password:
+        print("Đăng nhập thành công!")
+        break
+    else:
+        print("Sai mật khẩu vui lòng nhập lại!")
+        count += 1
+else:
+        print("Tài khoản bị khóa tạm thời!")
+
