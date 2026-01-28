734dde445556507e7aff58b367442cf203f86888
Nguyen Thanh Dat <nguyenthanhdat21072006@gmail.com>
2026-01-28 10:30:52 +0700

    Bài 2

---
+++ Dat_24DH260033/BaiKiemTra_1/BaiKiemTra_2.py
@@ -0,0 +1,13 @@
+matkhau="gay"
+solan = 1
+
+while solan <= 3:
+    nhap = input("Nhập mật khẩu: ")
+    if nhap == matkhau:
+        print("Đăng nhập thành công")
+        break
+    else:
+        print("Đăng nhập sai. Vui lòng nhập lại.")
+        solan += 1
+else:
+    print("Ban da nhap sai qua 3 lan, tai khoan bi khoa!")
