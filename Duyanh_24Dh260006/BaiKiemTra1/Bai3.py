1ff841180b1c3a55b92bf56c40bb60be139511ea
Duyanh_nhom7 <Duyanh666sh@gmail.com>
2026-01-28 10:42:44 +0700

    bài ktra 1 bai 1

---
+++ Duyanh_24Dh260006/BaiKiemTra1/Bai3.py
@@ -0,0 +1,9 @@
+
+n = int(input("Nhập số nguyên dương n (>1): "))
+
+for i in range(2, n):
+    if n % i == 0:
+        print("Không phải số nguyên tố")
+        break
+else:
+    print("Là số nguyên tố")
