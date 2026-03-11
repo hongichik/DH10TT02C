834dda5cd3603afc188c7fba5dee51d22e25d2e7
MINH NGUYET <vinhveo10082006@gmail.com>
2026-03-11 10:45:23 +0700

    BAI 11/3

---
+++ vinh_24DH260185/BAI2.py
@@ -0,0 +1,11 @@
+def TamGiacVuong(n):
+    for i in range(n):
+        print("*"*i)
+
+TamGiacVuong(5)
+print("-----------------")
+def TamGiacVuong(n):
+    for i in range(n):
+        print("*"*(n-1), end=" ")
+    print
+TamGiacVuong
