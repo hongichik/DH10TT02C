3a63dcebe4b7ea51f310a3d1b3e5aec7cc679533
Duyanh_nhom7 <Duyanh666sh@gmail.com>
2026-03-11 10:56:44 +0700

    bài ktra 1 bai 3

---
+++ Duyanh_24Dh260006/ngay28/Bai 2.py
@@ -0,0 +1,13 @@
+
+
+for n in range(1,10,2):
+    print(n,end=' ')
+#Xuong dong
+print()
+# Bai tap 1-100 bao nhieu so chia het cho 3
+# k  dem
+k=0
+for n in range(1,100):
+    if n % 3 == 0:
+        k+=1
+print("Tu 1-100 co:",k,"so chia het cho 3")
