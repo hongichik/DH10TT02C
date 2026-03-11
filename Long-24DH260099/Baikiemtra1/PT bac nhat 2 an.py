5c23c7a5b64bc3accf3000cc5ee5d784b31dacb5
TLong <nhatlonghl2512@gmail.com>
2026-03-11 10:28:45 +0700

    gui bai pt bac nhat 2 an

--- Long-24DH260099/Baikiemtra1/PT bac nhat 2 an.py
+++ Long-24DH260099/Baikiemtra1/PT bac nhat 2 an.py
@@ -1,4 +1,4 @@
-def pt(a, b, c):
+def pt2an(a, b, c):
     if a!= 0:
         x = c/a
         y = 0
@@ -20,4 +20,4 @@ a = float(input("nhap a"))
 b = float(input("nhap b"))
 c = float(input("nhap c"))
 
-pt(a, b, c)
+pt2an(a, b, c)
