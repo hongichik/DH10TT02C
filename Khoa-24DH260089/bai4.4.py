d3204a0f8f3c410c94e1bfe69f0123c270705985
khoa-nhom1 <khoa@gmail.com>
2026-03-18 12:45:15 +0700

    bài thực hành

---
+++ Khoa-24DH260089/bai4.4.py
@@ -0,0 +1,11 @@
+gio_hang = ["banh mi, sua, trung, bo"]
+gio_hang.insert(2, "gao")
+print(gio_hang)
+
+if "trung" in gio_hang:
+    print("Co trung trong gio hang")
+else:
+    print("Khong co trung trong gio hang.")
+
+    gio_hang.sort()
+    print(gio_hang)
