ef1805eee779a63ed0b774859796b1fce618a10a
TLong <nhatlonghl2512@gmail.com>
2026-03-18 10:06:10 +0700

    Gui bai tap

--- Long-24DH260099/Bai4_1.py
+++ Long-24DH260099/Bai4_1.py
@@ -1,7 +1,17 @@
-Traicay = ["cam", "quýt", "bưởi", "chanh", "quất"]
-Traicay.append("nho")
-print(Traicay)
-Traicay[1]="xoài"
-print(Traicay)
-Traicay.pop(len(Traicay)-1)
-print(Traicay)
+ca_nhan = {
+    "ten": "Long",
+    "tuoi": 20,
+    "nghe_nghiep": "Sinh viên",
+}
+
+print(ca_nhan)
+
+ca_nhan["email"] = "Nhatlonghl2512@gmail.com"
+
+print(ca_nhan)
+
+ca_nhan["tuoi"] = 25
+print(ca_nhan)
+
+ca_nhan.pop("nghe_nghiep")
+print(ca_nhan)
