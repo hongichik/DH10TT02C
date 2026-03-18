6a778a5511e90476d9988ad46c26cddda6c28dbe
duong-nhom10 <a@gmail.com>
2026-03-18 08:58:47 +0700

    bài 4

---
+++ Duong_24DH260027/bai4_1.py
@@ -0,0 +1,5 @@
+trai_cay = ["cam", "quýt", "bưởi", "chanh", "táo"]
+trai_cay.append("nho")
+trai_cay[1]="xoài"
+trai_cay.pop(len(trai_cay)-1)
+print(trai_cay)
---
+++ Duong_24DH260027/bai4_2.py
@@ -0,0 +1,5 @@
+ca_nhan={"ten":"Duc Duong", "tuoi": 20, "nghe_nghiep":"Sinh vien"}
+ca_nhan["email"]="duongvipubqn@gmail.com"
+ca_nhan["tuoi"]=21
+ca_nhan.pop("nghe_nghiep")
+print(ca_nhan)
