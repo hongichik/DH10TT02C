70b6782e8d9012ab3174d0dec0f779f85503d268
Hieu-nhom8 <vuminhhieu.hk@gmail.com>
2026-03-23 01:09:21 +0700

    gui bai 4 va 5

---
+++ Hieu-24DH260051/bai4_4.py
@@ -0,0 +1,9 @@
+mua_do=["sua", "banhmi", "trung", "nuoc"]
+mua_do[1]= "gao"
+print(mua_do)
+if "trung" in mua_do:
+    print("co trung")
+else:
+    print("khong co trung")
+mua_do.sort()
+print(mua_do)
---
+++ Hieu-24DH260051/bai4_5.py
@@ -0,0 +1,8 @@
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+list_moi = list1 + list2
+list_nhandoi= []
+for i in list_moi:
+    list_nhandoi.append(i * 2)
+print(list_moi)
+print(list_nhandoi)
