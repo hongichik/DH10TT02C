f72735993d68dfb4fd1e6d2eb413a3a619693bd6
LeNam <lequocnam23082006@gmail.com>
2026-03-23 20:39:05 +0700

    bài tập

---
+++ Nam-24DH260116/Bài 5.1.py
@@ -0,0 +1,4 @@
+nam = open("demo.txt", "w", encoding="utf-8")
+nam.write("day la mot ban demo\n")
+nam.write("dang hoc ve file trong python\n")
+nam.close()
---
+++ Nam-24DH260116/Bài thực hành/Bài 4.6.py
@@ -0,0 +1,11 @@
+sinh_vien = {
+    "id":"sv",
+    "name": "Nam",
+    "age": 20,
+    "gpa": 8.5
+}
+sinh_vien['major'] = "CNTT"
+sinh_vien['gpa'] += 0.5
+print(sinh_vien)
+print(sinh_vien.keys())
+print(sinh_vien.values())
