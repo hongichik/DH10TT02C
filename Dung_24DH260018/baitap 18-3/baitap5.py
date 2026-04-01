68fe9842fb7fcc0f017e145ea66ae741603caf3f
Dung_24DH2618 <hoangbcdung@gmail.com>
2026-04-01 10:16:17 +0700

    baitap5.py

---
+++ Dung_24DH260018/baitap 18-3/baitap5.py
@@ -0,0 +1,21 @@
+# B5: Tạo dictionary thông tin sinh viên
+student = {
+    "id": "SV001",
+    "name": "An",
+    "age": 20,
+    "gpa": 8.5
+}
+print("Ban đầu:", student)
+
+# Thêm ngành học
+student["major"] = "CNTT"
+print("Sau khi thêm major:", student)
+
+# Tăng điểm trung bình lên 0.5
+student["gpa"] = student["gpa"] + 0.5
+print("Sau khi tăng GPA:", student)
+
+# In tất cả key và value
+print("Thông tin sinh viên:")
+for key, value in student.items():
+    print(key, ":", value)
