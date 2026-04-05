6bdfd95840e3bee1cb6f658a70ac5958d0a1b508
trung-nhom5 <ngoductrung10706@gmail.com>
2026-04-05 20:23:20 +0700

    up

---
+++ trung_24DH260177/baitap/bt4.1.py
@@ -0,0 +1,7 @@
+trai_cay =["cam","xoai","buoi","le","tao"]
+trai_cay.append("nho")
+print(trai_cay)
+trai_cay[0]="mit"
+print(trai_cay)
+trai_cay.pop(len(trai_cay)-1)
+print(trai_cay)
--- trung_24DH260177/baitap/bt4.9.py
+++ trung_24DH260177/baitap/bt4.9.py
@@ -1,5 +1,17 @@
-file =open("demo.txt","w",encoding="utf-8")
-file.writelines("hello, this is a demo file.")
-file.write("Chung ta dang hoc vè file trong python")
-file.writelines(["\nDong 1\n","Dong 2\n","Dong 3\n"])
-file.close()
+#Tạo 3 dictionary
+danh_sach=[
+    {"name":"An","age":20,"score":8.0,},
+    {"name":"Bình","age":21,"score":9.5,},
+    {"name":"Chi","age":19,"score":8.5,}
+    ]
+#Tìm và in thông tin sinh viên có điểm cao nhất
+top_danh_sach=max(danh_sach,key=lambda x:x["score"])
+# print(top_danh_sach)
+#Tăng tuổi của tất cả sinh viên lên 1
+for nhan_vien in danh_sach:
+    nhan_vien["age"]+= 1
+    print(nhan_vien)
+#In danh sách mới sau khi cập nhập
+print(danh_sach)
+for s in danh_sach:
+     print(s)
